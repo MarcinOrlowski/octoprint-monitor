402eea1d31d66e6cf99c8501f47ceb3b1c104021
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2020-12-01 00:53:00 +0100

    Updated helper scripts

--- bin/pack.sh
+++ bin/pack.sh
@@ -9,11 +9,16 @@ fi
 
 declare -r pkg_name=$(cat "${src_dir}/metadata.desktop" | grep X-KDE-PluginInfo-Name | awk '{split($0,a,"="); print a[2]}')
 declare -r base_name=$(echo "${pkg_name}" | awk '{cnt=split($0,a,"."); print a[cnt]}')
-declare -r plasmoid_name="${base_name}.plasmoid"
+declare -r pkg_version=$(cat "${src_dir}/metadata.desktop" | grep X-KDE-PluginInfo-Version | awk '{split($0,a,"="); print a[2]}')
+declare -r plasmoid_path="../"
+declare -r plasmoid_name="${base_name}-${pkg_version}.plasmoid"
 
 echo "PKG_NAME: ${pkg_name}"
+echo " VERSION: ${pkg_version}"
 echo "PLASMOID: ${plasmoid_name}"
 
-(cd "${src_dir}" && zip -q -r "../${plasmoid_name}" *)
-ls -ld *.plasmoid
+pushd "${src_dir}" > /dev/null
+zip -q -r "${plasmoid_path}/${plasmoid_name}" *
+ls -ld "${plasmoid_path}/${plasmoid_name}"
+popd > /dev/null
 
