070cda680f72849d75cc0a77e946aea4388c3d89
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2020-12-03 21:27:18 +0100

    install helper uses /tmp for its plasmoid now

--- bin/install.sh
+++ bin/install.sh
@@ -10,7 +10,7 @@ fi
 declare -r pkg_name=$(cat src/metadata.desktop | grep X-KDE-PluginInfo-Name | awk '{split($0,a,"="); print a[2]}')
 declare -r base_name=$(echo "${pkg_name}" | awk '{cnt=split($0,a,"."); print a[cnt]}')
 declare -r pkg_version=$(cat "${src_dir}/metadata.desktop" | grep X-KDE-PluginInfo-Version | awk '{split($0,a,"="); print a[2]}')
-declare -r plasmoid_path="../"
+declare -r plasmoid_path="/tmp/"
 declare -r plasmoid_name="${base_name}-${pkg_version}.plasmoid"
 
 echo "PKG_NAME: ${pkg_name}"
@@ -24,9 +24,9 @@ popd > /dev/null
 
 declare -r user_home_dir="$(eval echo "~${USER}")"
 if [[ -d "${user_home_dir}/.local/share/plasma/plasmoids/${pkg_name}" ]]; then
-	kpackagetool5 --upgrade ${plasmoid_name}
+	kpackagetool5 --upgrade "${plasmoid_path}/${plasmoid_name}"
 else
-	kpackagetool5 --install ${plasmoid_name}
+	kpackagetool5 --install "${plasmoid_path}/${plasmoid_name}"
 fi
 
 kquitapp5 plasmashell 
