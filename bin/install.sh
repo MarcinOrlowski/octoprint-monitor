9bc61ef1d798061a0945b94ae4465370b70af75e
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2020-12-03 21:45:55 +0100

    Replaced cat invocation with redirection

--- bin/install.sh
+++ bin/install.sh
@@ -7,9 +7,9 @@ if [[ ! -d "${src_dir}" ]]; then
 	exit 1
 fi
 
-declare -r pkg_name=$(cat src/metadata.desktop | grep X-KDE-PluginInfo-Name | awk '{split($0,a,"="); print a[2]}')
+declare -r pkg_name=$(grep X-KDE-PluginInfo-Name < "${src_dir}/metadata.desktop" | awk '{split($0,a,"="); print a[2]}')
 declare -r base_name=$(echo "${pkg_name}" | awk '{cnt=split($0,a,"."); print a[cnt]}')
-declare -r pkg_version=$(cat "${src_dir}/metadata.desktop" | grep X-KDE-PluginInfo-Version | awk '{split($0,a,"="); print a[2]}')
+declare -r pkg_version=$(grep X-KDE-PluginInfo-Version < "${src_dir}/metadata.desktop" | awk '{split($0,a,"="); print a[2]}')
 declare -r plasmoid_path="/tmp/"
 declare -r plasmoid_name="${base_name}-${pkg_version}.plasmoid"
 
@@ -17,7 +17,12 @@ echo "PKG_NAME: ${pkg_name}"
 echo " VERSION: ${pkg_version}"
 echo "PLASMOID: ${plasmoid_name}"
 
-pushd "${src_dir}" > /dev/null
+tmp="$(mktemp -d "/tmp/${base_name}.XXXXXX")"
+cp -a "${src_dir}"/* "${tmp}"
+
+echo -e "var version=\"${pkg_version}\"" > "${tmp}/contents/js/version.js"
+
+pushd "${tmp}" > /dev/null
 zip -q -r "${plasmoid_path}/${plasmoid_name}" *
 ls -ld "${plasmoid_path}/${plasmoid_name}"
 popd > /dev/null
@@ -29,6 +34,6 @@ else
 	kpackagetool5 --install "${plasmoid_path}/${plasmoid_name}"
 fi
 
-kquitapp5 plasmashell 
+kquitapp5 plasmashell
 kstart5 plasmashell
 
--- bin/release.sh
+++ bin/release.sh
@@ -7,9 +7,9 @@ if [[ ! -d "${src_dir}" ]]; then
 	exit 1
 fi
 
-declare -r pkg_name=$(cat "${src_dir}/metadata.desktop" | grep X-KDE-PluginInfo-Name | awk '{split($0,a,"="); print a[2]}')
+declare -r pkg_name=$(grep X-KDE-PluginInfo-Name < "${src_dir}/metadata.desktop" | awk '{split($0,a,"="); print a[2]}')
 declare -r base_name=$(echo "${pkg_name}" | awk '{cnt=split($0,a,"."); print a[cnt]}')
-declare -r pkg_version=$(cat "${src_dir}/metadata.desktop" | grep X-KDE-PluginInfo-Version | awk '{split($0,a,"="); print a[2]}')
+declare -r pkg_version=$(grep X-KDE-PluginInfo-Version < "${src_dir}/metadata.desktop" | awk '{split($0,a,"="); print a[2]}')
 declare -r plasmoid_path="$(pwd)"
 declare -r plasmoid_name="${base_name}-${pkg_version}.plasmoid"
 
@@ -17,15 +17,13 @@ echo "PKG_NAME: ${pkg_name}"
 echo " VERSION: ${pkg_version}"
 echo "PLASMOID: ${plasmoid_name}"
 
-tmp="$(mktemp -d "/tmp/${base_name}.XXXXXX")"
-
-echo "${tmp}"
-cp -a "${src_dir}"/* "${tmp}"
-
 if [[ -f "${plasmoid_path}/${plasmoid_name}" ]]; then
 	echo "*** File already exists: ${plasmoid_path}/${plasmoid_name}"
 	exit 1
 fi
+
+tmp="$(mktemp -d "/tmp/${base_name}.XXXXXX")"
+cp -a "${src_dir}"/* "${tmp}"
 
 op_api_url=
 op_api_key=
@@ -38,8 +36,9 @@ cat "${cfg_template_file}" | sed -e "s/{OCTOPRINT_API_URL}/${op_api_url}/g" | se
 rm -vf "${cfg_template_file}"
 echo -e "var version=\"${pkg_version}\"" > "${tmp}/contents/js/version.js"
 
-zip -q -r "${plasmoid_path}/${plasmoid_name}" *
+zip -q -r "${plasmoid_path}/${plasmoid_name}" -- *
 ls -ld "${plasmoid_path}/${plasmoid_name}"
 
 popd > /dev/null
 
+rm -rf "${tmp}"
